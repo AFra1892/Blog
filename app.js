d9a51eac37f55dfdb9c925c7d888ba8197c4defa
amir <mashhadifarahani1@gmail.com>
2023-09-01 14:13:27 +0330

    test

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
  
   res.render('home' ,{data:homeStartingContent});
 });
